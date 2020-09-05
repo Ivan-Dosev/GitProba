edcae9b716ae437a320d93176430e4f3f4c2c616
Ivan <biobeehive.com>
2020-09-05 08:03:04 +0300

    Initial Commit

---
+++ GitProba/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitProba
+//
+//  Created by Ivan Dosev Dimitrov on 5.09.20.
+//  Copyright © 2020 Ivan Dosev Dimitrov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
